e58c2e0ad3dc5a26664be31d80e3ff5fc4602ad2
Ramon Fried <rfried.dev@gmail.com>
2019-06-19 07:40:19 +0300

    shunting fix

--- src/shunting-yard.c
+++ src/shunting-yard.c
@@ -5,7 +5,7 @@
 
 #include "shunting-yard.h"
 #include "stack.h"
-
+#include "bitwise.h"
 #include <ctype.h>
 #include <math.h>
 #include <stdbool.h>
@@ -295,81 +295,6 @@ uint64_t pop_num(Stack **operands)
 	uint64_t x = *pointer;
 	free((void *)pointer);
 	return x;
-}
-
-static int binary_scanf(const char *buf, uint64_t *val)
-{
-	uint64_t value = 0;
-
-	/* Skip the leading b */
-	buf++;
-
-	while (*buf) {
-		switch (*buf) {
-
-		case '0':
-			value <<= 1;
-			break;
-		case '1':
-			value <<= 1;
-			value++;
-			break;
-		default:
-			return 0;
-		}
-		buf++;
-	}
-
-	*val = value;
-
-	return 1;
-}
-
-static int base_scanf(const char *buf, int base, uint64_t *value)
-{
-	int ret = 0;
-
-	switch (base) {
-	case 10:
-		ret = sscanf(buf, "%lu", value);
-		break;
-	case 16:
-		ret = sscanf(buf, "%lx", value);
-		break;
-	case 8:
-		ret = sscanf(buf, "%lo", value);
-		break;
-	case 2:
-		ret = binary_scanf(buf, value);
-		break;
-	default:
-		fprintf(stderr, "Unknown base\n");
-		break;
-	}
-
-	if (ret == EOF || !ret) {
-		fprintf(stderr, "Couldn't parse number: %s\n", buf);
-		return 1;
-	}
-
-	return 0;
-}
-
-static int parse_input(const char *input, uint64_t *val)
-{
-	int base;
-
-	if (tolower(input[0]) == 'b')
-		base = 2;
-	else if (input[0] == '0')
-		if (input[1] == 'x' || input[1] == 'X')
-			base = 16;
-		else
-			base = 8;
-	else
-		base = 10;
-
-	return base_scanf(input, base, val);
 }
 
 Status push_number(const char *value, Stack **operands)
