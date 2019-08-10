eb3be240407d6f06444ac7c40b6674200bdf66ca
Ramon Fried <rfried.dev@gmail.com>
2019-08-10 21:08:25 +0300

    Complete the help screen

--- src/help.c
+++ src/help.c
@@ -5,13 +5,38 @@
 #include "bitwise.h"
 
 static const char *help_header = PACKAGE " " VERSION;
-static const char *modes = "Interactive mode: type numbers in different bases and directly manipulate bits\n"\
-			    "Command mode: type expressions or commands\n";
-static const char *navigation = "Interactive mode: To move around use the arrow keys, or use vi key bindings ('h|j|k|l')\n";
+static const char *interactive_mode = "Type numbers in different bases and directly manipulate bits.\n" \
+				       "To move around use the arrow keys, or use vi key bindings ('h|j|k|l').\n" \
+				       "In the binary field, toggle a bit using 'SPACE'.\n" \
+				       "You can jump a byte forward using 'w' and backwards one byte using 'b'.\n\n" \
+				       "Additional key mapping:\n" \
+				       "'F1' Show this help screen.\n" \
+				       "'~'  Perform logical NOT.\n" \
+				       "'<'  Perform left shift.\n" \
+				       "'>'  Perform right shift.\n\n" \
+				       "'!'  Set bit width to 8bit.\n" \
+				       "'@'  Set bit width to 16bit.\n" \
+				       "'$'  Set bit width to 32bit.\n" \
+				       "'*'  Set bit width to 64bit.\n" \
+				       "':'  Switch to command mode.\n" \
+				       "'q'  Exit back to shell.\n";
+
+static const char *command_mode = "Type expressions or commands, result will be appended to history window.\n" \
+				   "Supported operators: (,),+, -, *, /, !, ~, <<_, >>_, BIT(_).\n" \
+				   "Precedence of operators is defined as C language precedence.\n\n" \
+				   "You can referr to the last result with '$' inside an expression.\n" \
+				   "To return to interactive mode type 'ESC'.\n\n" \
+				   "Supported commands:\n" \
+				   "'help'      Show this help screen.\n" \
+				   "'clear'     Clear the history window.\n" \
+				   "'width []'  Set the bit width to [8 | 16 | 32 | 64]\n" \
+				   "'output []' Set the default output for results [decimal | hex | octal | binary | all]\n" \
+				   "'q'         Exit back to shell.\n";
 
 void show_help(void)
 {
 	WINDOW *help_win;
+	int ch;
 
 	/* Create full screen window */
 	help_win = newwin(0, 0, 0, 0);
@@ -25,24 +50,35 @@ void show_help(void)
 
 	wmove(help_win, 1, 0);
 	if (g_has_color)
-		wattron(help_win, A_UNDERLINE | COLOR_PAIR(1));
-	wprintw(help_win, "Modes:\n");
+		wattron(help_win, A_UNDERLINE | COLOR_PAIR(3));
+	wprintw(help_win, "Interactive Mode:\n\n");
+	if (g_has_color)
+		wattroff(help_win, A_UNDERLINE | COLOR_PAIR(3));
+
+	wprintw(help_win, "%s\n", interactive_mode);
 	if (g_has_color)
 		wattroff(help_win, A_UNDERLINE | COLOR_PAIR(1));
-	wprintw(help_win, "%s\n", modes);
 
 	if (g_has_color)
-		wattron(help_win, A_UNDERLINE | COLOR_PAIR(1));
-	wprintw(help_win, "Navigation:\n");
+		wattron(help_win, A_UNDERLINE | COLOR_PAIR(3));
+	wprintw(help_win, "Command Mode:\n\n");
 	if (g_has_color)
-		wattroff(help_win, A_UNDERLINE | COLOR_PAIR(1));
-	wprintw(help_win, "%s", navigation);
+		wattroff(help_win, A_UNDERLINE | COLOR_PAIR(3));
+	wprintw(help_win, "%s", command_mode);
 
 	wrefresh(help_win);
 	refresh();
-	wgetch(help_win);
+	ch = wgetch(help_win);
 
 	werase(help_win);
 	wrefresh(help_win);
 	delwin(help_win);
+
+	if (ch == 'q' || ch == 'Q')
+		g_leave_req = true;
+
+	if (ch == KEY_RESIZE) {
+		rl_resize_terminal();
+		show_help();
+	}
 }
