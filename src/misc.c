f1ea563d69e708b0fa43f67f4dfcc9a1f3ae697f
Gustavo Costa <xfgusta@gmail.com>
2023-08-16 12:13:59 -0300

    Fix binary prefix parsing

--- src/misc.c
+++ src/misc.c
@@ -215,7 +215,7 @@ int parse_input(const char *input, uint64_t *val)
 	if (tolower(input[0]) == 'b')
 		base = 2;
 	else if (input[0] == '0')
-		if (tolower(input[1] == 'b'))
+		if (tolower(input[1]) == 'b')
 			base = 2;
 		else if (input[1] == 'x' || input[1] == 'X')
 			base = 16;
